eb3fd9ce4630265c204b0a91c1ef31e8e0f447c8
Andrés <jsparanoguy@gmail.com>
2017-11-09 11:13:55 +0100

    fix database connection

--- server/models/index.ts
+++ server/models/index.ts
@@ -1,13 +1,16 @@
+import * as path from 'path'
 const Sequelize = require('sequelize')
 const env = 'dev'
-const config = require('./../../config.json').base[env]
+const config_path = path.join(process.cwd(), 'config.json')
+const json = require(config_path)
+const config = json.database.dev
 
 export const db = new Sequelize(
   config.database,
   config.user,
   config.password, {
     host: config.hostname,
-    dialect: 'mysql',
+    dialect: config.driver,
   },
 )
 
