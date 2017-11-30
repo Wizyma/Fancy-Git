4e1a90ed609e3d3f875e337be88509c355c16d8b
DOUX <mickael.doux@cartegie.com>
2017-11-30 15:37:34 +0100

    Add models/index.ts for sequelize

---
+++ server/models/index.ts
@@ -0,0 +1,15 @@
+import * as path from 'path'
+const Sequelize = require('sequelize')
+const env = 'dev'
+const config_path = path.join(process.cwd(), 'config.json')
+const json = require(config_path)
+const config = json.database.dev
+
+export const db = new Sequelize(
+    config.database,
+    config.user,
+    config.password, {
+        host: config.hostname,
+        dialect: config.driver,
+    },
+)
