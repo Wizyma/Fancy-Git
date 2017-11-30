c5abc470c0f544dc521e0efaafd9a329c9db99a4
Andrés <jsparanoguy@gmail.com>
2017-11-30 14:50:39 +0100

    user info

--- client/components/user.js
+++ client/components/user.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react'
 import { api } from '../utils/api'
 import { Loading } from './loading'
-// import { BuildResultUsersInfo } from './subcomponents/user_info'
+import { BuildResultUsersInfo } from './subcomponents/user_info'
 
 export class User extends Component{
     constructor(props){
         super(props)
         this.state = {
-         
+            name: props.location.state.name,
             datas: null,
           }
         console.log(props)
     }
 
     componentDidMount() {
-        api.getInfoUser('jsparanoguy')
+        api.getInfoUser(this.state.name)
             .then(datas => {
                 this.setState({ datas })
               })
@@ -30,6 +30,7 @@ export class User extends Component{
         return(
             <div>
             <h1>Hello World</h1>
+           {this.state.datas  && <BuildResultUsersInfo props={this.state.datas} />} 
             </div>
         )
     }
